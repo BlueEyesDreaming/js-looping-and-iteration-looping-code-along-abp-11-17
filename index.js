4197f960e03c7d9470db20da6fae5dc3d728c954
Ashton Reed <dreamergirl411@gmail.com>
2017-12-01 04:30:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,5 +13,5 @@ function tailsNeverFails() {
   while (tailsNeverFails()) {}
     counter++
   }
-  return `You got ${counter} tails in a row!`
+  console.log(`You got ${counter} tails in a row!`)
 }
