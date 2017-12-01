d8690b5515c96fed33bd8c2ffc743badf52523a1
Ashton Reed <dreamergirl411@gmail.com>
2017-12-01 04:09:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function printBadges(element) {
+function printBadges(arrayNames) {
   for (let i = 0; i < element.length; i++)
-  console.log(`Welcome ${element[i]}! You are employee #${i}.`)
+  console.log(`Welcome ${arrayNames[i]}! You are employee #${i+1}.`)
 }
